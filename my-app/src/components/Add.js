120b937ae7b80a52805aed9490f141b10887052a
aditya301200 <103925035+aditya301200@users.noreply.github.com>
2023-01-21 22:36:49 +0530

    ui correction of add.js

--- my-app/src/components/Add.js
+++ my-app/src/components/Add.js
@@ -5,6 +5,8 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import '../App.css'
+import SaveIcon from "@mui/icons-material/Save";
+import Button from "@mui/material/Button";
 
 const Add = ({employees, setEmployees, setIsAdding}) => {
 
@@ -14,54 +16,114 @@ const Add = ({employees, setEmployees, setIsAdding}) => {
     //TODO
   }
 
+
   return (
     <div>
       <form onSubmit={handleAdd}>
-        <h1>Add Employees</h1>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          Add Employees
+        </h1>
         <Box
           sx={{
-            maxWidth: "100%",
+            maxWidth: "50%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginInline: "auto",
           }}
         >
           <TextField
             fullWidth
             label="First Name"
             className="fullWidth"
+            InputLabelProps={{ className: "textField" }}
+            sx={{
+              input: { color: "#fff" },
+              marginBottom: "20px",
+              "& .MuiOutlinedInput-root": {
+                "& > fieldset": { borderColor: "white" },
+              },
+            }}
           />
           <TextField
             fullWidth
             label="Last Name"
             className="fullWidth"
             type="text"
+            InputLabelProps={{ className: "textField" }}
+            sx={{
+              input: { color: "#fff" },
+              marginBottom: "20px",
+              "& .MuiOutlinedInput-root": {
+                "& > fieldset": { borderColor: "white" },
+              },
+            }}
           />
           <TextField
             fullWidth
             label="Email"
             className="fullWidth"
             type="email"
+            InputLabelProps={{ className: "textField" }}
+            sx={{
+              input: { color: "#fff" },
+              marginBottom: "20px",
+              "& .MuiOutlinedInput-root": {
+                "& > fieldset": { borderColor: "white" },
+              },
+            }}
           />
           <TextField
             fullWidth
             label="Salary"
             className="fullWidth"
             type="number"
+            InputLabelProps={{ className: "textField" }}
+            sx={{
+              input: { color: "#fff" },
+              marginBottom: "20px",
+              "& .MuiOutlinedInput-root": {
+                "& > fieldset": { borderColor: "white" },
+              },
+            }}
           />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
               label="Date"
               value={value}
-              className='fullWidth'
               onChange={(newValue) => {
                 setValue(newValue);
               }}
               renderInput={(params) => (
                 <TextField
                   {...params}
-                  helperText={params?.inputProps?.placeholder}
+                  InputLabelProps={{ className: "textField" }}
+                  sx={{
+                    svg: { color: "#fff" },
+                    input: { color: "#fff" },
+                    marginBottom: "20px",
+                    "& .MuiOutlinedInput-root": {
+                      "& > fieldset": { borderColor: "white" },
+                    },
+                  }}
                 />
               )}
             />
           </LocalizationProvider>
+
+          <Button
+            variant="contained"
+            color="success"
+            style={{ marginBlock: "20px" }}
+          >
+            <SaveIcon />
+            Add Employee
+          </Button>
         </Box>
       </form>
     </div>
